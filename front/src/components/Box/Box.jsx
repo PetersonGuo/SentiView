b82a12d651f2f7ee38ed4ba278ff15bb048a2ec9
Eugene Zhang <zhang.eug@gmail.com>
2023-01-15 11:35:24 -0500

    small edits

--- front/src/components/Box/Box.jsx
+++ front/src/components/Box/Box.jsx
@@ -18,51 +18,62 @@ function Box(props) {
     }
   };
 
-	const closeModal = () => {
-		const modal = document.querySelector(".modal");
-		modal.classList.remove("block");
-		modal.classList.add("hidden");
-	};
+  const closeModal = () => {
+    const modal = document.querySelector(".modal");
+    modal.classList.remove("block");
+    modal.classList.add("hidden");
+  };
 
-	return (
-			<div className="mt-auto mb-auto flex justify-center">
-				<div className="box flex">
-					<div className="gradient self-start flex justify-center" id={props.id}>
-						<h2 className="title-text">{props.title}</h2>
-					</div>
-					<div className="flex flex-col w-full h-full justify-center">
-						{props.datas.map((data, index) => (
-								<ol key={index} className="list-disc">
-									<li
-											id="press"
-											className="text-[#FFD178] font-bold text-2xl transition-transform hover:translate-x-5 hover:cursor-pointer"
-											onClick={addBox}
-									>
-										{data.name}
-									</li>
-									<li className="text-white list-none">{data.count} occurences</li>
-								</ol>
-						))}
-					</div>
-					<div className="modal hidden fixed left-0 top-0 mt-1/2 w-full h-screen bg-darkBg flex items-center">
-						<div className="border-2 border-solid border-[#FFD178] mx-auto box flex justify-center">
-							<div className="gradient self-start flex items-center">
-								<span
-										onClick={closeModal}
-										className="float-right font-bold text-[#FFD178] hover:text-[#FFD178] hover:no-underline hover:cursor-pointer ml-10 absolute">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         strokeWidth={1.5} stroke="currentColor" className="w-14 h-14">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
-										</svg>
-                                </span>
-								<h2 className="title-text mx-auto">{props.title}</h2>
-							</div>
-							<h1 id="info" className="p-[20px] text-[#FFD178] text-center"></h1>
-						</div>
-					</div>
-				</div>
-			</div>
-	);
+  return (
+    <div className="mt-auto mb-auto flex justify-center">
+      <div className="box flex">
+        <div className="gradient self-start flex justify-center" id={props.id}>
+          <h2 className="title-text">{props.title}</h2>
+        </div>
+        <div className="flex flex-col w-full h-full justify-center">
+          {props.datas.map((data, index) => (
+            <ol key={index} className="list-disc">
+              <li
+                id="press"
+                className="text-[#FFD178] font-bold text-2xl transition-transform hover:translate-x-5 hover:cursor-pointer"
+                onClick={addBox}
+              >
+                {data.name}
+              </li>
+              <li className="text-white list-none">{data.count} occurences</li>
+            </ol>
+          ))}
+        </div>
+        <div className="modal hidden fixed left-0 top-0 mt-1/2 w-full h-screen bg-darkBg flex items-center">
+          <div className="border-2 border-solid border-[#FFD178] mx-auto box flex justify-center">
+            <div className="gradient self-start flex items-center">
+              <span
+                onClick={closeModal}
+                className="float-right font-bold text-[#FFD178] hover:text-[#FFD178] hover:no-underline hover:cursor-pointer ml-10 absolute"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-14 h-14"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </span>
+              <h2 className="title-text mx-auto">{props.title}</h2>
+            </div>
+            <h1 id="info" className="p-[20px] text-[#FFD178] text-center"></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Box;
