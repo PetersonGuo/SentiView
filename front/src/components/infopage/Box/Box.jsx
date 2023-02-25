845965f09d51e1208164b36191237282b51804a2
Peterson Guo <petersonguo@gmail.com>
2023-02-25 01:27:37 -0500

    Better Modal

--- front/src/components/infopage/Box/Box.jsx
+++ front/src/components/infopage/Box/Box.jsx
@@ -4,30 +4,18 @@ import loading from './loading.gif';
 
 function Box(props) {
   const data = Array.from(props.data);
-
   const addBox = (e) => {
-    const modal = document.querySelector(".modal");
-    modal.classList.add("block");
-    modal.classList.remove("hidden");
+    props.setModalOpen(true);
+    props.setTitle(props.title);
 
-    const switchTitle = document.getElementById("switch");
-    switchTitle.innerText = props.title;
-
-    const info = document.getElementById("info");
-    info.innerText = "";
-
+    let modalText = "";
     const rightData = e.target.innerText;
     for (let i = 0; i < props.sents.length; i++) {
       if (props.sents[i].includes(rightData)) {
-        info.innerText += `- ${props.sents[i]} \n`;
+        modalText += `- ${props.sents[i]} \n`;
       }
     }
-  };
-
-  const closeModal = () => {
-    const modal = document.querySelector(".modal");
-    modal.classList.remove("block");
-    modal.classList.add("hidden");
+    props.setModalText(modalText);
   };
 
   return (
@@ -51,35 +39,6 @@ function Box(props) {
               </ol>
           ))}
         </div> : <img className={"m-auto"} src={loading} alt={"Loading..."} width={50} />}
-        <div className="modal hidden fixed left-0 top-0 mt-1/2 w-full h-screen bg-darkBg flex items-center">
-          <div className="border-2 border-solid border-[#FFD178] mx-auto box flex justify-center">
-            <div className="gradient self-start flex items-center">
-              <span
-                onClick={closeModal}
-                className="float-right font-bold text-[#FFD178] hover:text-[#FFD178] hover:no-underline hover:cursor-pointer ml-10 absolute"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-14 h-14"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </span>
-              <h2 id="switch" className="title-text mx-auto">
-                {props.title}
-              </h2>
-            </div>
-            <h1 id="info" className="p-[20px] text-[#FFD178] text-center h-full w-full overflow-y-scroll"></h1>
-          </div>
-        </div>
       </div>
     </div>
   );
--- front/src/components/infopage/Infopage.js
+++ front/src/components/infopage/Infopage.js
@@ -1,9 +1,12 @@
 import Box from "./Box/Box";
 import Nav from "../Nav/Nav";
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
 export function InfoPage(props) {
   const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [title, setTitle] = useState(props.title);
+
   useEffect(() => {
     async function loadTable() {
       let posArr = [];
@@ -25,8 +28,11 @@ export function InfoPage(props) {
       setPosSent(props.data.PositiveInput);
       setNegSent(props.data.NegativeInput);
     }
+
     setLoading(false);
-    loadTable().then(() => {setLoading(props.data.PositiveList !== undefined);});
+    loadTable().then(() => {
+      setLoading(props.data.PositiveList !== undefined);
+    });
   }, [props.data]);
 
   const infoRef = useRef(null);
@@ -47,15 +53,53 @@ export function InfoPage(props) {
     positiveSent: [],
   });
 
+  const [modalText, setModalText] = useState("");
+
   return (
-    <div ref={infoRef} id="info-page" className="fadeIn">
-      <Nav />
-      <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <div className="main ">
-          <Box title="Positive" data={posData} sents={posSent} loading={loading}></Box>
-          <Box id="b2" title="Negative" data={negData} sents={negSent} loading={loading}></Box>
+      <div ref={infoRef} id="info-page" className="fadeIn">
+        <Nav/>
+        <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <div className="main">
+            {modalOpen ? <div className="modal flex items-center">
+                  <div className="border-2 border-solid border-[#FFD178] m-auto box flex justify-center">
+                    <div className="gradient self-start flex items-center">
+              <span
+                  onClick={() => setModalOpen(false)}
+                  className="float-right font-bold text-[#FFD178] hover:text-[#FFD178] hover:no-underline hover:cursor-pointer ml-10 absolute"
+              >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-14 h-14"
+                >
+                  <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </span>
+                      <h2 className="title-text mx-auto">
+                        {title}
+                      </h2>
+                    </div>
+                    <h1 id="info"
+                        className="p-[20px] text-[#FFD178] text-center h-full w-full overflow-y-scroll whitespace-pre-line">{modalText}</h1>
+                  </div>
+                </div>
+                :
+                <>
+                  <Box title="Positive" data={posData} sents={posSent} loading={loading} setModalOpen={setModalOpen}
+                       setTitle={setTitle} setModalText={setModalText}/>
+                  <Box id="b2" title="Negative" data={negData} sents={negSent} loading={loading}
+                       setModalOpen={setModalOpen} setTitle={setTitle} setModalText={setModalText}/>
+                </>
+            }
+          </div>
         </div>
       </div>
-    </div>
   );
 }
