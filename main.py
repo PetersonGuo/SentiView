6557d75a95b1b63ddad97c5599b03b25d2f30cd4
petersonguo <petersonguo@gmail.com>
2024-02-08 22:50:55 -0500

    Add a bit of web scraping

--- main.py
+++ main.py
@@ -1,94 +1,120 @@
+import time
 import cohere
 import os
-import json
 from stopwords import stopwords
 import training_data
 from cohere.responses.classify import Example
 from flask import Flask, request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+
+
+options = Options()
+options.add_argument("--headless=new")
+
 
 app = Flask(__name__)
 
+
+
 @app.route("/acceptData", methods=["POST"])
 def get_data():
-  print(request.headers)
+    print(request.headers)
 
-  request_data = request.form.to_dict()
-  data_type = int(request_data['type'])
+    request_data = request.form.to_dict()
+    data_type = int(request_data['type'])
 
-  inputs = []
+    inputs = []
 
-  if data_type == 0:
-    pass
-  elif data_type == 1:
-    inputs = request_data["data"].split(",")
-    print(inputs)
-  elif data_type == 2:
-    inputs = request.files["file"].read().decode("utf-8").split(",")
-    for i in range(len(inputs)):
-      inputs[i] = inputs[i].strip()
+    if data_type == 0:
+        driver = webdriver.Chrome(options=options)
+        driver.get(request_data["data"])
+        time.sleep(3)
+        try:
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'More')]"))
+            more_buttons = driver.find_elements(By.XPATH, "//button[contains(text(),'More')]")
 
-  co = cohere.Client(f'{os.getenv("COHERE_KEY")}')
+            for button in more_buttons:
+                try:
+                    driver.execute_script("arguments[0].scrollIntoView();", button)
+                    button.click()
+                except Exception as click_error:
+                    print("Could not click button:", click_error)
+        except Exception as e:
+            print("Error:", e)
+        inputs = [str(response.text) for response in driver.find_elements(By.CSS_SELECTOR, "div.MyEned")]
+        print(inputs)
+        driver.quit()
+    elif data_type == 1:
+        inputs = request_data["data"].split(",")
+        print(inputs)
+    elif data_type == 2:
+        inputs = request.files["file"].read().decode("utf-8").split(",")
+        for i in range(len(inputs)):
+            inputs[i] = inputs[i].strip()
 
-  pos_reviews = []
-  neg_reviews = []
+    co = cohere.Client(f'{os.getenv("COHERE_KEY")}')
 
-  examples=map(lambda x: Example(text=x[0], label=x[1]), training_data.training_data)
+    pos_reviews = []
+    neg_reviews = []
 
-  response = co.classify(
-    model='large',
-    inputs=inputs,
-    examples=examples,
-  )
+    examples = map(lambda x: Example(text=x[0], label=x[1]), training_data.training_data)
 
+    response = co.classify(
+        model='large',
+        inputs=inputs,
+        examples=examples,
+    )
 
-  def class_reviews():
-    res_class = response.classifications
+    def class_reviews():
+        res_class = response.classifications
 
-    in_and_pred = zip(inputs, res_class)
+        in_and_pred = zip(inputs, res_class)
 
-    for i in in_and_pred:
-      pred = i[1].predictions[0]
-      if pred == "neutral" or pred == "positive":
-        pos_reviews.append(i[0])
-      else:
-        neg_reviews.append(i[0])
+        for i in in_and_pred:
+            pred = i[1].predictions[0]
+            if pred == "neutral" or pred == "positive":
+                pos_reviews.append(i[0])
+            else:
+                neg_reviews.append(i[0])
 
-    return pos_reviews, neg_reviews
+        return pos_reviews, neg_reviews
 
-  def tokenize(pos_reviews, neg_reviews):
-    pos_words = {}
-    neg_words = {}
-    for i in pos_reviews:
-      for j in co.tokenize(i).token_strings:
-        if pos_words.get(j) == None:
-          pos_words[j] = 1
-        else:
-          pos_words[j] += 1
+    def tokenize(pos_reviews, neg_reviews):
+        pos_words = {}
+        neg_words = {}
+        for i in pos_reviews:
+            for j in co.tokenize(i).token_strings:
+                if pos_words.get(j) is None:
+                    pos_words[j] = 1
+                else:
+                    pos_words[j] += 1
 
-    for i in neg_reviews:
-      for j in co.tokenize(i).token_strings:
-        if neg_words.get(j) == None:
-          neg_words[j] = 1
-        else:
-          neg_words[j] += 1
+        for i in neg_reviews:
+            for j in co.tokenize(i).token_strings:
+                if neg_words.get(j) is None:
+                    neg_words[j] = 1
+                else:
+                    neg_words[j] += 1
 
-    return pos_words, neg_words
+        return pos_words, neg_words
 
-  pos, neg = class_reviews()
-  pos_dict, neg_dict = tokenize(pos, neg)
-  cleaned_pos_dict = dict(sorted({k.strip():v for (k, v) in pos_dict.items() if not k.strip().lower() in
-  stopwords}
-  .items(), key=lambda item: item[1], reverse=True))
-  cleaned_neg_dict = dict(sorted({k.strip():v for (k, v) in neg_dict.items() if not k.strip().lower() in
-  stopwords}
-  .items(), key=lambda item: item[1], reverse=True))
+    pos, neg = class_reviews()
+    pos_dict, neg_dict = tokenize(pos, neg)
+    cleaned_pos_dict = dict(sorted({k.strip(): v for (k, v) in pos_dict.items() if not k.strip().lower() in stopwords}
+                                   .items(), key=lambda item: item[1], reverse=True))
+    cleaned_neg_dict = dict(sorted({k.strip(): v for (k, v) in neg_dict.items() if not k.strip().lower() in stopwords}
+                                   .items(), key=lambda item: item[1], reverse=True))
 
-  return {
-      'PositiveList' : list(cleaned_pos_dict.items())[:5],
-      'NegativeList' : list(cleaned_neg_dict.items())[:5],
-      'PositiveInput' : pos,
-      'NegativeInput' : neg
-  }
+    return {
+        'PositiveList': list(cleaned_pos_dict.items())[:5],
+        'NegativeList': list(cleaned_neg_dict.items())[:5],
+        'PositiveInput': pos,
+        'NegativeInput': neg
+    }
+
 
 if __name__ == "__main__":
-  app.run(debug=True, port=4000)
+    app.run(debug=True, port=4000)
